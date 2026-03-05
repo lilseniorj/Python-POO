aca9b99b0b9c6517b6d7e7d3eb33cae8811f1a9c
lilseniorj <jesusvarguer18@gmail.com>
2026-03-05 16:53:45 -0500

    Encapsulación en Python: atributos privados y métodos getter/setter

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Book:
         self.popular = popular
         self.isbn = isbn
         self.disponible = disponible
-        self.historial_prestamos = []
+        self.__historial_prestamos = 0
 
     def __str__(self):
         return f"{self.title} por {self.author}, disponible: {self.disponible}"
@@ -14,21 +14,32 @@ class Book:
     def prestar(self):
         if self.disponible:
             self.disponible = False
-            self.historial_prestamos.append(self)
-            return f"{self.title}: prestado exitosamente."
+            self.__historial_prestamos += 1
+            return f"{self.title}: prestado exitosamente. Total préstamos: {self.__historial_prestamos}"
+        return f"{self.title} no está disponible"
 
     def devolver(self):
         self.disponible = True
         return f"{self.title}, devuelto y disponible nuevamente"
 
     def is_popular(self):
-        return self.historial_prestamos.count(self) > 5
+        return self.historial_prestamos > 5
+
+    def get__historial_prestamos(self):
+        return self.__historial_prestamos
+
+    def set_historial_prestamos(self, historial_prestamos):
+        self.__historial_prestamos = historial_prestamos
 
 my_book = Book("100 años de soledad", " Gabriel Garcia Marquez", "very popular", "1212121212", True)
 another_book = Book("El principito", "Saint-Exupéry", "is popular", "141414141414", True)
 
-my_book.prestar()
-my_book.devolver()
+my_book.set_historial_prestamos(10)
+print(my_book.get__historial_prestamos())
+
+# print(my_book.prestar())
+# print(my_book.prestar())
+# print(my_book.devolver())
 
 catalogo = (my_book, another_book)
 
