5b698329f26d4f10b992283080ec0d131a88059a
lilseniorj <jesusvarguer18@gmail.com>
2026-03-06 19:43:05 -0500

    Polimorfismo en Python

--- usuarios.py
+++ usuarios.py
@@ -1,3 +1,10 @@
+from typing import Protocol
+
+class SolicitanteProtocol(Protocol):
+	def  solicitar_libro(self, titulo: str) -> str:
+		"""Metodo que debe implementar cualquier solicitante"""
+		...
+
 class Usuario:
 	def __init__(self, name, id):
 		self.name = name
@@ -22,13 +29,6 @@ class Estudiante(Usuario):
 			return (
 				f"No puedes prestar mas libro, Limite alcanzado {self.limite_libros}"
 			)
-	def devolver_libro(self, titulo):
-		if titulo in self.libros_prestados:
-			self.libros_prestados.remove(titulo)
-			self.limite_libros -= 1
-			return f"El libro {titulo} ha sido devuelto exitosamente, su limite de libros ahora es de {self.limite_libros}, sus libros prestados son {self.libros_prestados}"
-		else:
-			return f"Este libro {titulo} no esta en su lista de libros prestados, devolución rechazada"
 
 class Profesor(Usuario):
 	def __init__(self, name, id):
@@ -42,12 +42,17 @@ class Profesor(Usuario):
 
 
 estudiante = Estudiante("Jesus", "123456", "Sistema")
+estudiante_1 = Estudiante("David", "1423456", "Salud")
 profesor = Profesor("Luis", "4123123")
 
-print(profesor.solicitar_libro("Python Basico"))
-print(profesor.solicitar_libro("Python Medio"))
-print(profesor.solicitar_libro("Python Profesional"))
-print(profesor.solicitar_libro("Python Experto"))
-print(estudiante.devolver_libro("Python Basico"))
+usuarios = [estudiante, profesor, estudiante_1, libro]
+
+from main import Libro
+
+libro = Libro("Titulo de prueba", "Autor de prueba", isbn="13123124")
+
+usuarios: list[SolicitanteProtocol] = [estudiante, estudiante_1, profesor, libro]
 
 
+for usuario in usuarios:
+	print(usuario.solicitar_libro("Tirulo del ejemplo"))
